f1f7d8b22e13924c13765d6ef20b22c7df398283
PP Solution <deepak.k@papayapalette.in>
2025-11-04 11:23:08 +0530

    v3

--- src/utils/mui_select_scroll_bar.js
+++ src/utils/mui_select_scroll_bar.js
@@ -1,6 +1,6 @@
-// constants.js
-export const customMenuProps = {
-  disablePortal: true,
-  disableScrollLock: true,
-  PaperProps: { style: { maxHeight: 200 } },
-};
+
+// export const customMenuProps = {
+//   disablePortal: true,
+//   disableScrollLock: true,
+//   PaperProps: { style: { maxHeight: 200 } },
+// };
